21e41bdc0878140cec604b8baa2a76b271fe5c70
unknown <info@johnnyramirez.es>
2022-11-15 09:29:22 +0100

    Resolución ejercicio IVA

---
+++ src/assets/js/app-1.js
@@ -0,0 +1,33 @@
+/*
+* Conectar archivo javascript al html de destino
+* A partir del precio de un producto y el IVA correspondiente, imprimir el precio total en nuestra ventana del navegador
+* * definir el contenedor o espacio que va a ocupar el texto
+* * acceder al contenedor desde javascript
+* * asignar los valores de precio y porcentaje de IVA
+* * calcular el importe del IVA a partir del precio y de la tasa de IVA
+* * calcular el precio total, sumando el importe del IVA al precio del producto
+* * componer el texto a mostrar
+* * insertar en el contenedor
+* * imprimirlo en pantalla
+*/
+
+let precioBase = 125;
+let porcentajeIva = 21;
+
+function calcularIva(precio, iva) {
+    let importeIva = precio * iva / 100;
+    return importeIva;
+}
+
+function calcularPrecio() {
+    let precioTotal = precioBase + calcularIva(precioBase, porcentajeIva);
+    return precioTotal;
+}
+
+function mostrarPrecio() {
+    let mensajeTxt = document.getElementById("ejercicioIva");
+    mensajeTxt.innerHTML = `Precio del producto: ${precioBase}, importe del IVA: ${porcentajeIva}%, <strong>precio total: ${calcularPrecio()}</strong>`;
+    calcularPrecio();
+}
+
+mostrarPrecio();

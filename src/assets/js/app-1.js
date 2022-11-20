b929b9fcedf7cac428af20445642a1598bbf59a2
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-20 18:55:59 +0100

    Incorporado formulario para recoger precio e IVA

--- src/assets/js/app-1.js
+++ src/assets/js/app-1.js
@@ -4,6 +4,7 @@
 * * definir el contenedor o espacio que va a ocupar el texto
 * * acceder al contenedor desde javascript
 * * recoger los valores de precio e IVA suministrados por el usuario
+* * comprobacion básica de si los valores proporcionados son números y, en caso negativo, convertirlos
 * * calcular el importe del IVA a partir del precio y de la tasa de IVA
 * * calcular el precio total, sumando el importe del IVA al precio del producto
 * * componer el texto a mostrar
@@ -12,24 +13,29 @@
 */
 
 function calcularIva(precio, iva) {
-    let precioBase = precio;
-    let ivaBase = iva;
-    let importeIva = precioBase * ivaBase / 100;
+    const importeIva = precio * iva / 100;
     return importeIva;
 }
 
 function calcularPrecio(precio, iva) {
-    let precioCalculado = precio;
-    let ivaCalculado = iva;
-    let precioTotal = precioCalculado + ivaCalculado;
+    const precioTotal = precio + calcularIva(precio, iva);
     return precioTotal;
+}
+
+function volverNumero(numero) {
+    if(!isNaN(numero)){
+        numero = Number(numero);
+    }
+    return numero;
 }
 
 function mostrarPrecioTotal() {
     let precioUsuario = document.getElementById('precioBase').value;
+    precioUsuario = volverNumero(precioUsuario);
     let ivaUsuario = document.getElementById('porcentajeIva').value;
-    let ivaCalculado = calcularIva(precioUsuario, ivaUsuario);
-    let precioTotal = calcularPrecio(precioUsuario, ivaCalculado);
-    let mensajeTxt = document.getElementById("ejercicioIva");
+    ivaUsuario = volverNumero(ivaUsuario);
+    const ivaCalculado = calcularIva(precioUsuario, ivaUsuario);
+    const precioTotal = calcularPrecio(precioUsuario, ivaCalculado);
+    const mensajeTxt = document.getElementById("ejercicioIva");
     mensajeTxt.innerHTML = `Precio del producto: ${precioUsuario}, importe del IVA: ${ivaUsuario}%, <strong>precio total: ${precioTotal}</strong>`;
 }
